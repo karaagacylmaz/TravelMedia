3fb39e7977f7018fdb73624f3f730f68f00effcd
yilmazkaraagac <karaagacylmaz@hotmail.com>
2022-01-24 01:51:56 +0300

    get data when ViewController reload

---
+++ TravelMedia/TableViewModel.swift
@@ -0,0 +1,14 @@
+//
+//  TableViewModel.swift
+//  TravelMedia
+//
+//  Created by Yılmaz Karaağaç on 1/24/22.
+//
+
+import Foundation
+
+class TableViewModel {
+    var id: UUID?
+    var name = ""
+    var index = 0
+}
--- TravelMedia/ViewController.swift
+++ TravelMedia/ViewController.swift
@@ -11,11 +11,18 @@ import CoreData
 class ViewController: UIViewController, UITableViewDelegate, UITableViewDataSource {
 
     @IBOutlet weak var tableView: UITableView!
+    
+    var mediaArray = [TableViewModel]()
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
         addPlusButtonToNavigationBar()
         configureTableView()
+        getData()
+    }
+    
+    override func viewWillAppear(_ animated: Bool) {
+        getData()
     }
     
     // MARK: - Plus button
@@ -35,15 +42,101 @@ class ViewController: UIViewController, UITableViewDelegate, UITableViewDataSour
     // the template of a cell
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         let cell = UITableViewCell()
-        cell.textLabel?.text = "test"
+        cell.textLabel?.text = mediaArray.first { $0.index == indexPath.row}?.name
         return cell
     }
     // cell count
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return 10
+        return mediaArray.count
+    }
+    
+    //MARK: - get data when view controller load
+    func getData() {
+        mediaArray.removeAll()
+        let context = getContext()
+        let request = getFetchRequest()
+        do {
+            let response = try context.fetch(request)
+            if response.count > 0 {
+                fillDataToModel(response)
+            }
+        } catch {
+            print("fetch error in get data")
+        }
+    }
+    
+    func fillDataToModel(_ response: [NSFetchRequestResult]) {
+        var counter = 0
+        for res in response as! [NSManagedObject] {
+            let mediaModel = TableViewModel()
+            if let id = res.value(forKey: "id") as? UUID {
+                mediaModel.id = id
+            }
+            
+            if let name = res.value(forKey: "galleryName") as? String {
+                mediaModel.name = name
+            }
+            
+            mediaModel.index = counter
+            counter += 1
+            self.mediaArray.append(mediaModel)
+            self.tableView.reloadData()
+        }
     }
     
     // MARK: - Core Data Usage
-
+    func getContext() -> NSManagedObjectContext {
+        let appDelegate = UIApplication.shared.delegate as! AppDelegate
+        let context = appDelegate.persistentContainer.viewContext
+        return context
+    }
+    
+    func getFetchRequest() -> NSFetchRequest<NSFetchRequestResult> {
+        let fetchRequest = NSFetchRequest<NSFetchRequestResult>(entityName: "Place")
+        fetchRequest.returnsObjectsAsFaults = false
+        return fetchRequest
+    }
+    
+    func removeData(_ context: NSManagedObjectContext, _ request: NSFetchRequest<NSFetchRequestResult>, _ id: String, _ index: Int) {
+        do {
+            let response = try context.fetch(request)
+            if response.count > 0 {
+                for res in response as! [NSManagedObject] {
+                    if let mediaId = res.value(forKey: "id") as? UUID {
+                        if mediaId.uuidString == id {
+                            context.delete(res)
+                            mediaArray.remove(at: index)
+                            self.tableView.reloadData()
+                            saveChanges(context)
+                        }
+                    }
+                }
+            }
+        } catch {
+            print("error in removeData")
+        }
+    }
+    
+    func saveChanges(_ context: NSManagedObjectContext) {
+        do {
+            try context.save()
+            print("success delete")
+        } catch {
+            print("error in savechanges")
+        }
+    }
+    
+    // MARK: - Remove Data From Screen And Core Data
+    func tableView(_ tableView: UITableView, commit editingStyle: UITableViewCell.EditingStyle, forRowAt indexPath: IndexPath) {
+        if editingStyle == .delete {
+            let context = getContext()
+            let request = getFetchRequest()
+            
+            let id = mediaArray.first { $0.index == indexPath.row}?.id?.uuidString
+            request.predicate = NSPredicate(format: "id = %@", id!)
+            
+            removeData(context, request, id!, indexPath.row)
+        }
+    }
 }
 
